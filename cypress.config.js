235a25bb1df5bbe53a3ce0ef0cb19380e809be14
Rishvanth K S <rishe9752@gmail.com>
2024-07-11 11:45:44 +0530

    fixed a bug(train selection) and code auto refactored by biome

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,24 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '7afdkj',
+	projectId: "7afdkj",
 
-  defaultCommandTimeout: 120000,
-  // video: true,
+	defaultCommandTimeout: 120000,
+	// video: true,
 
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      on('task', {
-        log(message) {
-          // Then to see the log messages in the terminal
-          //   cy.task("log", "my message");
-          console.log(message + '\n\n');
-          return null;
-        },
-      });
-    },
-    chromeWebSecurity: false,
-    experimentalModifyObstructiveThirdPartyCode: true
-  },
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+			on("task", {
+				log(message) {
+					// Then to see the log messages in the terminal
+					//   cy.task("log", "my message");
+					console.log(`${message}\n\n`);
+					return null;
+				},
+			});
+		},
+		chromeWebSecurity: false,
+		experimentalModifyObstructiveThirdPartyCode: true,
+	},
 });
-
-
